af480fcf4c117d638466bf873cfa407f0d7872cb
Valentin Baca <valbaca@gmail.com>
2023-05-17 21:14:14 -0700

    adding navigation bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,33 @@
-import Main from "./components/Main"
+import { NavigationContainer } from '@react-navigation/native'
+import Main from './components/Main'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { Entypo, MaterialIcons } from '@expo/vector-icons'
+
+const Tab = createMaterialBottomTabNavigator()
 
 export default function App() {
-  return (<Main/>)
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Have"
+          component={Main}
+          options={{
+            tabBarIcon: () => (
+              <Entypo name="bowl" size={20} color="midnightblue" />
+            )
+          }}
+        />
+        {/* <Tab.Screen
+          name="Need"
+          component={Main}
+          options={{
+            tabBarIcon: () => (
+              <Entypo name="shopping-cart" size={20} color="midnightblue" />
+            )
+          }}
+        /> */}
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
---
+++ README.md
@@ -0,0 +1,18 @@
+# chicken-stock
+A simple app to keep your food in stock!
+
+## Useful links
+
+[Primary Inspiration](https://dev.to/reenydavidson/building-a-to-do-list-with-react-native-and-styled-components-2148) with [code: native-todo](https://github.com/ReenyDavidson/native-todo/blob/main/Components/TodoList.js)
+
+- [Expo](https://expo.dev/)
+- [FlatList](https://reactnative.dev/docs/flatlist)
+- [Expo Icons](https://icons.expo.fyi/), esp Entypo
+- [React Native docs](https://reactnative.dev/docs/components-and-apis)
+- [Tab navigation](https://reactnavigation.org/docs/tab-based-navigation/)
+- [styled-components](https://styled-components.com/)
+
+One-time references:
+
+- [Setup TypeScript](https://docs.expo.dev/guides/typescript/)
+- [TextView + value](https://stackoverflow.com/a/75353575/158886)
